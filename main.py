96425d5045f7aac617034ac939498f2c6618608a
Aman <prasadaman2000@gmail.com>
2021-11-12 23:04:15 -0500

    email collection

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
 
 
 app = FastAPI()
@@ -13,3 +14,16 @@ async def root():
     with open("sometext", "r") as f:
         s = f.read()
         return {"message": s}
+
+
+@app.get("/emailCollect/{email}")
+async def emailCollect(email: str):
+    with open("emails", "a") as f:
+        f.write(email + "\n")
+    return "good"
+
+@app.get("/emails")
+async def emails():
+    with open("emails", "a") as f:
+        emails = f.read()
+        return {"emails": emails}
